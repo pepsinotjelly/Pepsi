14fb554293f8eb5ae638ae09a3c94d37dce338fe
pepsinotjelly <1637597600@qq.com>
2022-04-23 19:23:45 +0800

    init window

---
+++ game/love_pancake_store/src/main/java/com/bubble/App.java
@@ -0,0 +1,22 @@
+package com.bubble;
+
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+
+import org.lwjgl.Version;
+import static org.lwjgl.glfw.Callbacks.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
